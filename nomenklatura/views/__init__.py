a870939d5b9f16ddcb89a09f02d387affe09fca9
Friedrich Lindenberg <friedrich@pudo.org>
2015-03-16 12:34:46 +0100

    Split out UI angular code a bit.

--- nomenklatura/views/__init__.py
+++ nomenklatura/views/__init__.py
@@ -1,13 +1,13 @@
-import os
-
-from flask import render_template, request
+from flask import request
 from formencode import Invalid
 from apikit import jsonify
 
-from nomenklatura.core import app, login_manager
+from nomenklatura.core import login_manager
 from nomenklatura.model import User
-from nomenklatura.views.upload import section as upload
+from nomenklatura.views.ui import app
 from nomenklatura.views.sessions_api import blueprint as sessions_api
+from nomenklatura.views.users_api import blueprint as users_api
+from nomenklatura.views.upload import section as upload
 from nomenklatura.views.datasets import section as datasets
 from nomenklatura.views.entities import section as entities
 from nomenklatura.views.reconcile import section as reconcile
@@ -56,32 +56,11 @@ def handle_invalid(exc):
     return jsonify(body, status=400)
 
 
+app.register_blueprint(sessions_api, url_prefix='/api/2')
+app.register_blueprint(users_api, url_prefix='/api/2')
+
 app.register_blueprint(upload, url_prefix='/api/2')
 app.register_blueprint(reconcile, url_prefix='/api/2')
-app.register_blueprint(sessions_api, url_prefix='/api/2')
 app.register_blueprint(datasets, url_prefix='/api/2')
 app.register_blueprint(entities, url_prefix='/api/2')
 app.register_blueprint(matching, url_prefix='/api/2')
-
-
-def angular_templates():
-    if app.config.get('ASSETS_DEBUG'):
-        return
-    partials_dir = os.path.join(app.static_folder, 'templates')
-    for (root, dirs, files) in os.walk(partials_dir):
-        for file_name in files:
-            file_path = os.path.join(root, file_name)
-            with open(file_path, 'rb') as fh:
-                yield ('/static/templates/%s' % file_path[len(partials_dir)+1:],
-                       fh.read().decode('utf-8'))
-
-
-@app.route('/entities')
-@app.route('/entities/<path:id>')
-@app.route('/datasets')
-@app.route('/datasets/<path:id>')
-@app.route('/profile')
-@app.route('/docs/<path:id>')
-@app.route('/')
-def index(**kw):
-    return render_template('app.html', angular_templates=angular_templates())
---
+++ nomenklatura/views/ui.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import render_template
+
+from nomenklatura.core import app
+
+
+def angular_templates():
+    partials_dir = os.path.join(app.static_folder, 'templates')
+    for (root, dirs, files) in os.walk(partials_dir):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            with open(file_path, 'rb') as fh:
+                sub_path = file_path[len(partials_dir) + 1:]
+                yield ('/static/templates/%s' % sub_path,
+                       fh.read().decode('utf-8'))
+
+
+@app.route('/entities')
+@app.route('/entities/<path:id>')
+@app.route('/datasets')
+@app.route('/datasets/<path:id>')
+@app.route('/profile')
+@app.route('/docs/<path:id>')
+@app.route('/')
+def index(**kw):
+    return render_template('app.html', angular_templates=angular_templates())
