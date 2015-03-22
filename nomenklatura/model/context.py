105cefab4dbaca5670237953c484f546326360a4
Friedrich Lindenberg <friedrich@pudo.org>
2015-03-22 21:36:31 +0100

    Refactor contexts to have more specific values.

--- nomenklatura/model/context.py
+++ nomenklatura/model/context.py
@@ -9,28 +9,34 @@ class Context(db.Model, CommonMixIn):
 
     __tablename__ = 'context'
 
-    url = db.Column(db.Unicode)
+    source_url = db.Column(db.Unicode)
     publisher = db.Column(db.Unicode)
     publisher_url = db.Column(db.Unicode)
-    user_submitted = db.Column(db.Boolean)
+    active = db.Column(db.Boolean)
 
     dataset_id = db.Column(db.String(KEY_LENGTH), db.ForeignKey('dataset.id'))
     dataset = db.relationship('Dataset', backref=db.backref('contexts',
                               lazy='dynamic', cascade='all, delete-orphan')) # noqa
+
+    user_id = db.Column(db.String(KEY_LENGTH), db.ForeignKey('user.id'),
+                        nullable=True)
+    user = db.relationship('User', backref=db.backref('contexts',
+                           lazy='dynamic', cascade='all, delete-orphan')) # noqa
 
     def to_dict(self):
         return {
             'id': self.id,
             'created_at': self.created_at,
             'updated_at': self.updated_at,
-            'url': self.url,
-            'user_submitted': self.user_submitted,
+            'source_url': self.source_url,
+            'active': self.active,
+            'user_id': self.user_id,
             'publisher': self.publisher,
             'publisher_url': self.publisher_url
         }
 
     def __repr__(self):
-        return u'<Context(%r, %r)>' % (self.id, self.url)
+        return u'<Context(%r, %r)>' % (self.id, self.source_url)
 
     def __unicode__(self):
         return self.id
