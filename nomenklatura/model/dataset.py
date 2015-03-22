cdafd11f995fe812b3b88255003034339c182a05
Friedrich Lindenberg <friedrich@pudo.org>
2015-03-22 17:10:01 +0100

    Clean out old stats code.

--- nomenklatura/model/dataset.py
+++ nomenklatura/model/dataset.py
@@ -23,24 +23,12 @@ class Dataset(db.Model, CommonMixIn):
                             lazy='dynamic')
 
     def to_dict(self):
-        # from nomenklatura.model.entity import Entity
-        # num_aliases = Entity.all(self).filter(Entity.canonical_id != None).count()
-        # num_review = Entity.all(self).filter_by(reviewed=False).count()
-        # num_entities = Entity.all(self).count()
-        # num_invalid = Entity.all(self).filter_by(invalid=True).count()
-
         return {
             'id': self.id,
             'slug': self.slug,
             'api_url': url_for('datasets.view', dataset=self.slug),
             'label': self.label,
             'owner': self.owner.to_dict(),
-            # 'stats': {
-            #     'num_aliases': num_aliases,
-            #     'num_entities': num_entities,
-            #     'num_review': num_review,
-            #     'num_invalid': num_invalid
-            # },
             'ignore_case': self.ignore_case,
             'match_aliases': self.match_aliases,
             'public': self.public,
