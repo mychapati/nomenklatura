a2a117faf82c49ff424704a5763ecff868b2c7a2
Friedrich Lindenberg <friedrich@pudo.org>
2015-03-28 21:12:28 +0100

    Re-write inference to run with one query.

--- nomenklatura/model/inference.py
+++ nomenklatura/model/inference.py
@@ -1,55 +1,58 @@
-# TODO, write this in SQL
 import logging
-from itertools import count
+from sqlalchemy import exists, and_
+from sqlalchemy.orm import aliased
 
 from nomenklatura.core import db
+from nomenklatura.schema import attributes
 from nomenklatura.model.statement import Statement
-from nomenklatura.model.schema import attributes
 
 log = logging.getLogger(__name__)
 
 
-def clear():
+def delete_inferred_statements(dataset):
     q = db.session.query(Statement)
     q = q.filter(Statement.inferred == True) # noqa
+    q = q.filter(Statement.dataset == dataset)
     q.delete()
 
 
-def clone(src, subject):
-    if subject == src.subject:
-        return
-    dst = Statement(src.dataset, subject, src.attribute, src.value,
-                    src.context)
-    dst.inferred = True
-    dst.created_at = src.created_at
-    db.session.add(dst)
+def generate_same_as_copies(dataset):
+    # TODO: this should be recursive, but that seems a bit too weird?
+    stmt = aliased(Statement)
+    sa_stmt = aliased(Statement)
+    ex_stmt = aliased(Statement)
+    q = db.session.query(sa_stmt.subject, stmt._attribute, stmt._value,
+                         stmt.context_id)
+    q = q.filter(and_(
+        stmt.dataset_id == dataset.id,
+        sa_stmt.dataset_id == dataset.id,
+        stmt._attribute != attributes.same_as.name,
+        sa_stmt._attribute == attributes.same_as.name,
+        sa_stmt._value == stmt.subject
+    ))
+
+    q = q.filter(~exists().where(and_(
+        ex_stmt.subject == sa_stmt.subject,
+        ex_stmt._attribute == stmt._attribute,
+        ex_stmt._value == stmt._value,
+        ex_stmt.context_id == sa_stmt.context_id,
+    )))
+
+    i = -1
+    for i, row in enumerate(q):
+        s, a, v, c = row
+        inf = Statement(dataset, s, attributes[a], None, None)
+        inf.inferred = True
+        inf._value = v
+        inf.context_id = c
+        db.session.add(inf)
+
+    log.info("Generated %s 'same_as' statement copies.", i + 1)
+    db.session.flush()
 
 
-def generate(mapping):
-    count = 0
-    q = db.session.query(Statement)
-    q = q.filter(Statement.subject == mapping.subject)
-    q = q.filter(Statement._attribute != attributes.same_as.name)
-    for stmt in q:
-        iq = db.session.query(Statement)
-        iq = iq.filter(Statement.subject == mapping._value)
-        iq = iq.filter(Statement._attribute == stmt._attribute)
-        iq = iq.filter(Statement._value == stmt._value)
-        iq = iq.filter(Statement.context_id == stmt.context_id)
-        iq = iq.filter(Statement.inferred == True) # noqa
-        if not iq.count():
-            log.info("Cloning %r", stmt)
-            clone(stmt, mapping._value)
-            count += 1
-    return count
-
-
-def infer():
-    clear()
-    for i in count(1):
-        log.info('Inference, iteration #%s', i)
-        q = db.session.query(Statement)
-        q = q.filter(Statement._attribute == attributes.same_as.name)
-        if not sum([generate(s) for s in q]):
-            break
+def infer(dataset):
+    delete_inferred_statements(dataset)
+    db.session.flush()
+    generate_same_as_copies(dataset)
     db.session.commit()
