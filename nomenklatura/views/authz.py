8c9875245c9d3773b9fc2fd7c26a5f2a121d8982
Friedrich Lindenberg <friedrich@pudo.org>
2015-03-29 23:10:35 +0200

    Refactor authz.

---
+++ nomenklatura/views/authz.py
@@ -0,0 +1,32 @@
+from flask.ext.login import current_user
+from werkzeug.exceptions import Forbidden
+
+from nomenklatura.core import app
+from nomenklatura.model.constants import READ, EDIT, MANAGE
+
+
+def logged_in():
+    return current_user.is_authenticated()
+
+
+def role():
+    if logged_in():
+        return current_user.system_role
+    return app.config.get('DEFAULT_ROLE')
+
+
+def system_manage():
+    return role() == MANAGE
+
+
+def system_edit():
+    return system_manage() or role() == EDIT
+
+
+def system_read():
+    return system_edit() or role() == READ
+
+
+def require(pred):
+    if not pred:
+        raise Forbidden("Sorry, you're not permitted to do this!")
