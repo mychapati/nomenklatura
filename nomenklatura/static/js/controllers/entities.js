f69fe50bc17aa95e62608642e2f382fa61aa8f4e
Annabel Church <bells.stuff+gitcommitme@gmail.com>
2015-04-30 20:34:20 +1200

    Simple edit

--- nomenklatura/static/js/controllers/entities.js
+++ nomenklatura/static/js/controllers/entities.js
@@ -15,5 +15,35 @@ nomenklatura.controller('EntitiesViewCtrl', ['$scope', '$routeParams', '$locatio
 
   $scope.entity = entity;
   $scope.schema = Meta.schema;
+  $scope.editing = false;
+
+  $scope.beginEdit = function(){
+    $scope.editedEntity = angular.copy($scope.entity);
+    $scope.editing = true;
+  };
+
+  $scope.saveEdit = function(){
+    $scope.editing = false;
+    $scope.entity = $scope.editedEntity;
+
+    $http.post($scope.entity.api_url, $scope.entity).
+    success(function(data, status, headers, config) {
+      $scope.editedEntity = null;
+    }).
+    error(function(data, status, headers, config) {
+      // display message to user
+    });
+
+    // spinny thing
+    // why not put?
+    // make save a factory service
+
+  };
+
+  $scope.cancelEdit = function(){
+    $scope.editing = false;
+    $scope.editedEntity = null;
+  };
 
 }]);
+
